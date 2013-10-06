ffe77654fecd8f4dc8099cb95e760ce4284e2242
James Titcumb <james@asgrim.com>
2013-10-06 14:53:17 +0100

    Removed old crap

--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-chdir(dirname(__DIR__));
-
-$loader = require_once('vendor/autoload.php');
-$loader->add('Asgrim', 'app/src');
-
-$app = new Asgrim\Application('app/config/config.yml');
-$app->run();
