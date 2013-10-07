ce1132a111e18901dadb283eb99b3184389f8202
James Titcumb <james@asgrim.com>
2013-10-07 21:38:17 +0100

    Added talks and books views

--- views/books.php
+++ views/books.php
@@ -1,5 +1,13 @@
 <?php require_once('header.php'); ?>
 
-...books!
+<p>This is a list of books I'm working on:</p>
+
+<ul>
+	<li>
+		<h3>Reusable Zend Framework 2 Modules</h3>
+		<p>This book shows you some good conventions and tips to writing reusable modules for Zend Framework 2. I explain a few general good OOP practices, layout and structure conventions and suggest a few good resources for researching.</p>
+		<p><strong>Buy here: <a href="https://leanpub.com/reusable-zf2-modules">https://leanpub.com/reusable-zf2-modules</a></strong></p>
+	</li>
+</ul>
 
 <?php require_once('footer.php'); ?>
--- views/talks.php
+++ views/talks.php
@@ -1,5 +1,23 @@
 <?php require_once('header.php'); ?>
 
-About my talks
+<p>This is a list of talks I've given:</p>
+
+<ul>
+	<li>
+		<h3>Errors, Exceptions & Logging (PHP Hampshire, 9th Oct '13)</h3>
+		<p>The talk is designed to give an entry-level introduction to how you should be handling errors, exceptions and how to effectively log in an application.</p>
+		<p><strong>Links:</strong> <a href="https://joind.in/9452">Joind.in</a></p>
+	</li>
+	<li>
+		<h3>Errors, Exceptions & Logging (PHPNW13 Uncon)</h3>
+		<p>A brief introduction to how to handle errors, exceptions and some effective ways to log them.</p>
+		<p><strong>Links:</strong> <a href="https://joind.in/9470">Joind.in</a></p>
+	</li>
+	<li>
+		<h3>Composer (PHP Hampshire, 11th Sep '13)</h3>
+		<p>A very quick introduction to the basics of Composer, what problems it solves, what it does, including a live demo.</p>
+		<p><strong>Links:</strong> <a href="https://joind.in/9341">Joind.in</a> | <a href="http://www.youtube.com/watch?v=nnDUSkvdvWg">YouTube</a></p>
+	</li>
+</ul>
 
 <?php require_once('footer.php'); ?>
