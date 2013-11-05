70695b339504cb83091434bd2340493676d8151b
James Titcumb <james@asgrim.com>
2013-11-05 22:49:49 +0000

    Added BrightonPHP talk

--- views/talks.php
+++ views/talks.php
@@ -4,6 +4,11 @@
 
 <ul>
 	<li>
+		<h3>Low Latency Logging (BrightonPHP, 18th Nov '13)</h3>
+		<p>Logging is an absolute must for any API or web application, but when starting out, questions such as "how can we do it without disrupting everything else" and "what is the easiest way to log" often come out. I'm going to explore a couple of infrastructure ideas to carry out what I call "high-performance, low-latency" logging to ensure minimal impact to the performance of the runtime application. The talk will show you that a really great logging architecture is a low-cost investment in your application that will definitely pay off in the long run.</p>
+		<p><strong>Links:</strong> <a href="https://joind.in/9928">Joind.in</a></p>
+	</li>
+	<li>
 		<h3>Errors, Exceptions & Logging (PHP Hampshire, 9th Oct '13)</h3>
 		<p>The talk is designed to give an entry-level introduction to how you should be handling errors, exceptions and how to effectively log in an application.</p>
 		<p><strong>Links:</strong> <a href="https://joind.in/9452">Joind.in</a></p>
