b8b130cefc569c2d51e4d8d5e752707c313d50a1
James Titcumb <james@asgrim.com>
2013-11-05 22:44:39 +0000

    Covert posts.php to PHP 5.3 format arrays

--- posts/posts.php
+++ posts/posts.php
@@ -1,23 +1,23 @@
 <?php
-return [
-	'phpunit-coverage' => [
+return array(
+	'phpunit-coverage' => array(
 		'date' => '2013-06-28',
 		'file' => '2013/2013-06-28-phpunit-coverage.md',
 		'title' => 'PHPUnit Coverage...',
-	],
-	'downgrading-ondrejphp5-to-ondrejphp5-oldstable' => [
+	),
+	'downgrading-ondrejphp5-to-ondrejphp5-oldstable' => array(
 		'date' => '2013-08-06',
 		'file' => '2013/2013-08-06-downgrading-ondrejphp5-to-ondrejphp5-oldstable.md',
 		'title' => 'Downgrading ondrej/php5 to ondrej/php5-oldstable',
-	],
-	'lightning-talk-on-composer-php-hants-sept-13' => [
+	),
+	'lightning-talk-on-composer-php-hants-sept-13' => array(
 		'date' => '2013-09-12',
 		'file' => '2013/2013-09-12-lightning-talk-on-composer-php-hants-sept-13.md',
 		'title' => 'Lightning Talk on Composer @ PHP Hants Sept 13',
-	],
-	'zf2-doctrine-objectselect-form-binding-custom-select-options' => [
+	),
+	'zf2-doctrine-objectselect-form-binding-custom-select-options' => array(
 		'date' => '2013-09-27',
 		'file' => '2013/2013-09-27-zf2-doctrine-objectselect-form-binding-custom-select-options.md',
 		'title' => 'ZF2 + Doctrine ObjectSelect + Form binding + Custom Select Options',
-	],
-];
+	),
+);
