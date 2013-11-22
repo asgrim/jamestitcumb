ef4b67a6a001df9589701f48eab3a6a9990fb2e6
James Titcumb <james@asgrim.com>
2013-11-22 11:59:56 +0000

    Added slides links

--- views/talks.php
+++ views/talks.php
@@ -6,22 +6,22 @@
 	<li>
 		<h3>Low Latency Logging (BrightonPHP, 18th Nov '13)</h3>
 		<p>Logging is an absolute must for any API or web application, but when starting out, questions such as "how can we do it without disrupting everything else" and "what is the easiest way to log" often come out. I'm going to explore a couple of infrastructure ideas to carry out what I call "high-performance, low-latency" logging to ensure minimal impact to the performance of the runtime application. The talk will show you that a really great logging architecture is a low-cost investment in your application that will definitely pay off in the long run.</p>
-		<p><strong>Links:</strong> <a href="https://joind.in/9928">Joind.in</a> | <a href="http://lanyrd.com/2013/brightonphp-november/">Lanyrd</a></p>
+		<p><strong>Links:</strong> <a href="https://joind.in/9928">Joind.in</a> | <a href="http://lanyrd.com/2013/brightonphp-november/">Lanyrd</a> | <a href="http://www.slideshare.net/asgrim1/low-latency-logging-brighton-php-18th-nov-2013">Slides</a></p>
 	</li>
 	<li>
 		<h3>Errors, Exceptions & Logging (PHP Hampshire, 9th Oct '13)</h3>
 		<p>The talk is designed to give an entry-level introduction to how you should be handling errors, exceptions and how to effectively log in an application.</p>
-		<p><strong>Links:</strong> <a href="https://joind.in/9452">Joind.in</a></p>
+		<p><strong>Links:</strong> <a href="https://joind.in/9452">Joind.in</a> | <a href="http://www.youtube.com/watch?v=NnhkNhM3aDQ">YouTube</a> | <a href="http://www.slideshare.net/asgrim1/errors-exceptions-logging-php-hants-oct-13">Slides</a></p>
 	</li>
 	<li>
 		<h3>Errors, Exceptions & Logging (PHPNW13 Uncon)</h3>
 		<p>A brief introduction to how to handle errors, exceptions and some effective ways to log them.</p>
-		<p><strong>Links:</strong> <a href="https://joind.in/9470">Joind.in</a></p>
+		<p><strong>Links:</strong> <a href="https://joind.in/9470">Joind.in</a> | <a href="http://www.slideshare.net/asgrim1/errors-exceptions-logging-phpnw13-uncon">Slides</a></p>
 	</li>
 	<li>
 		<h3>Composer (PHP Hampshire, 11th Sep '13)</h3>
 		<p>A very quick introduction to the basics of Composer, what problems it solves, what it does, including a live demo.</p>
-		<p><strong>Links:</strong> <a href="https://joind.in/9341">Joind.in</a> | <a href="http://www.youtube.com/watch?v=nnDUSkvdvWg">YouTube</a></p>
+		<p><strong>Links:</strong> <a href="https://joind.in/9341">Joind.in</a> | <a href="http://www.youtube.com/watch?v=nnDUSkvdvWg">YouTube</a> | <a href="http://www.slideshare.net/asgrim1/composer-tutorial-php-hants-sept-13-26138484">Slides</a></p>
 	</li>
 </ul>
 
