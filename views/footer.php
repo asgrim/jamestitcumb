d1c04b86648247fbd54bcefd37338df6d9e6a7a3
James Titcumb <james@asgrim.com>
2013-12-09 16:46:46 +0000

    Added comments links

--- views/footer.php
+++ views/footer.php
@@ -7,11 +7,24 @@
   _gaq.push(['_trackPageview']);
 
   (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
 
+</script>
+
+<script type="text/javascript">
+/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+var disqus_shortname = 'asgrim'; // required: replace example with your forum shortname
+
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function () {
+	var s = document.createElement('script'); s.async = true;
+	s.type = 'text/javascript';
+	s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+	(document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());
 </script>
 
 </body>
--- views/posts.php
+++ views/posts.php
@@ -4,7 +4,10 @@
 <article style="margin-bottom: 60px;">
 	<div class="article-header">
 		<h1><a href="/posts/<?php echo $slug; ?>"><?php echo $post['title']; ?></a></h1>
-		<p style="font-weight: bold; font-style: italic;">Posted on <?php echo date('jS F Y', strtotime($post['date'])); ?></p>
+		<p style="font-weight: bold; font-style: italic;">
+			Posted on <?php echo date('jS F Y', strtotime($post['date'])); ?>
+			| <a href="/posts/<?php echo $slug; ?>#disqus_thread">View comments</a>
+		</p>
 	</div>
 	<hr />
 	<div class="article-body">
