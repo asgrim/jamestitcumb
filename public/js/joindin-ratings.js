69cff8c898319deb9999f1deec0940b2b433d27f
James Titcumb <james@asgrim.com>
2014-10-21 08:53:30 +0100

    Added magic Joindin ratings from JI API

---
+++ public/js/joindin-ratings.js
@@ -0,0 +1,15 @@
+(function(){
+	$(document).ready(function () {
+		$('.joindin').each(function () {
+			var el = $(this);
+			var href = el.attr('href');
+			var talkId = parseInt(href.substr(href.lastIndexOf('/') + 1));
+			$.getJSON('http://api.joind.in/v2.1/talks/' + talkId + '?callback=?', function (data) {
+				var rating = data.talks[0].average_rating;
+				if (rating > 0) {
+					el.after(' <img src="/images/ji-ratings/rating-' + rating + '.gif" />');
+				}
+			});
+		});
+	});
+})();
--- views/talks.php
+++ views/talks.php
@@ -1,4 +1,10 @@
-<?php require_once('header.php'); ?>
+<?php
+$additionalHeader = <<<EOF
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="/js/joindin-ratings.min.js"></script>
+EOF;
+require_once('header.php');
+?>
 
 <p>Upcoming talks:</p>
 
@@ -26,12 +32,12 @@
 	<li>
 		<h3>Practical Message Queueing Using RabbitMQ (PHPNW14 Uncon, 4th Oct '14)</h3>
 		<p>RabbitMQ is a message broker – an application that allows communication between applications by way of a message queuing system. In this talk, we look at some of the basic concepts of RabbitMQ and how it can help effectively scale your applications.</p>
-		<p><strong>Links:</strong> <a href="https://joind.in/talk/view/12144">Joind.in</a> | <a href="http://www.slideshare.net/asgrim1/141004-what-rabbit-mq-can-do-for-you-phpnw14-uncon">Slides</a></p>
+		<p><strong>Links:</strong> <a href="https://joind.in/talk/view/12144" class="joindin">Joind.in</a> | <a href="http://www.slideshare.net/asgrim1/141004-what-rabbit-mq-can-do-for-you-phpnw14-uncon">Slides</a></p>
 	</li>
 	<li>
 		<h3>Low Latency Logging with RabbitMQ (Brno PHP Conference 2014, 4th Sep '14)</h3>
 		<p>Logging is an absolute must for any API or web application, but when starting out, questions such as "how can we do it without disrupting everything else" and "what is the easiest way to log" often come up. We’re going to examine a tried and tested method to carry out high-performance, low-latency logging using the power of RabbitMQ to ensure minimal impact to the performance of your runtime application. The talk will show you that a really great logging architecture is a low-cost investment in your application that will definitely pay off in the long run.</p>
-		<p><strong>Links:</strong> <a href="https://www.brnophp.cz/conference-2014">Brno PHP Conference</a> | <a href="https://joind.in/11825">Joind.in</a> | <a href="http://www.slideshare.net/asgrim1/low-latency-logging-with-rabbitmq-brno-php-cz-20th-sep-2014">Slides</a></p>
+		<p><strong>Links:</strong> <a href="https://www.brnophp.cz/conference-2014">Brno PHP Conference</a> | <a href="https://joind.in/11825" class="joindin">Joind.in</a> | <a href="http://www.slideshare.net/asgrim1/low-latency-logging-with-rabbitmq-brno-php-cz-20th-sep-2014">Slides</a></p>
 	</li>
 	<li>
 		<h3>Low Latency Logging with RabbitMQ (PHP London, 4th Sep '14)</h3>
@@ -51,42 +57,42 @@
 	<li>
 		<h3>Dip Your Toes in the Sea of Security (PHP Dorset, 2nd Jun '14)</h3>
 		<p>Security is an enormous topic, and it’s really, really complicated. If you’re not careful, you’ll find yourself vulnerable to any number of attacks which you definitely don’t want to be on the receiving end of. This talk will give you just a taster of the vast array of things there is to know about security in modern web applications, such as writing secure PHP web applications and securing a Linux server. Whether you are writing anything beyond a basic brochure website, or even developing a complicated business web application, this talk will give you insights to some of the things you need to be aware of.</p>
-		<p><strong>Links:</strong> <a href="https://joind.in/11353">Joind.in</a> | <a href="http://vimeo.com/97645043">Vimeo</a> | <a href="http://www.slideshare.net/asgrim1/dip-your-toes-in-the-sea-of-security-php-dorset">Slides</a></p>
+		<p><strong>Links:</strong> <a href="https://joind.in/11353" class="joindin">Joind.in</a> | <a href="http://vimeo.com/97645043">Vimeo</a> | <a href="http://www.slideshare.net/asgrim1/dip-your-toes-in-the-sea-of-security-php-dorset">Slides</a></p>
 	</li>
 	<li>
 		<h3>What RabbitMQ Can Do For You (Nomad PHP, 22nd May '14)</h3>
 		<p>RabbitMQ is a message broker – an application that allows communication between applications by way of a message queuing system. In this talk, we look at some of the basic concepts of RabbitMQ and how it can help effectively scale your applications.</p>
-		<p><strong>Links:</strong> <a href="https://joind.in/11350">Joind.in</a> | <a href="https://www.youtube.com/watch?v=4lDSwfrfM-I">YouTube</a> | <a href="https://www.slideshare.net/asgrim1/rabbit-mq-nomad-php-may-2014">Slides</a></p>
+		<p><strong>Links:</strong> <a href="https://joind.in/11350" class="joindin">Joind.in</a> | <a href="https://www.youtube.com/watch?v=4lDSwfrfM-I">YouTube</a> | <a href="https://www.slideshare.net/asgrim1/rabbit-mq-nomad-php-may-2014">Slides</a></p>
 	</li>
 	<li>
 		<h3>What RabbitMQ Can Do For You (PHP Hampshire, 9th Apr '14)</h3>
 		<p>A lightning talk that gives a quick introduction as to what RabbitMQ is for and what it can do for your applications.</p>
-		<p><strong>Links:</strong> <a href="https://joind.in/11174">Joind.in</a> | <a href="http://www.youtube.com/watch?v=sY_cKzwyC5k">YouTube</a> | <a href="http://www.slideshare.net/asgrim1/rabbit-mq-32447680">Slides</a></p>
+		<p><strong>Links:</strong> <a href="https://joind.in/11174" class="joindin">Joind.in</a> | <a href="http://www.youtube.com/watch?v=sY_cKzwyC5k">YouTube</a> | <a href="http://www.slideshare.net/asgrim1/rabbit-mq-32447680">Slides</a></p>
 	</li>
 	<li>
 		<h3>What RabbitMQ Can Do For You (PHPNE14 Uncon, 18th Mar '14)</h3>
 		<p>An introduction to what RabbitMQ is and what it can do for your applications.</p>
-		<p><strong>Links:</strong> <a href="https://joind.in/10937">Joind.in</a> | <a href="http://www.slideshare.net/asgrim1/rabbit-mq-32447680">Slides</a></p>
+		<p><strong>Links:</strong> <a href="https://joind.in/10937" class="joindin">Joind.in</a> | <a href="http://www.slideshare.net/asgrim1/rabbit-mq-32447680">Slides</a></p>
 	</li>
 	<li>
 		<h3>Low Latency Logging (BrightonPHP, 18th Nov '13)</h3>
 		<p>Logging is an absolute must for any API or web application, but when starting out, questions such as "how can we do it without disrupting everything else" and "what is the easiest way to log" often come out. I'm going to explore a couple of infrastructure ideas to carry out what I call "high-performance, low-latency" logging to ensure minimal impact to the performance of the runtime application. The talk will show you that a really great logging architecture is a low-cost investment in your application that will definitely pay off in the long run.</p>
-		<p><strong>Links:</strong> <a href="https://joind.in/9928">Joind.in</a> | <a href="http://lanyrd.com/2013/brightonphp-november/">Lanyrd</a> | <a href="http://www.slideshare.net/asgrim1/low-latency-logging-brighton-php-18th-nov-2013">Slides</a></p>
+		<p><strong>Links:</strong> <a href="https://joind.in/9928" class="joindin">Joind.in</a> | <a href="http://lanyrd.com/2013/brightonphp-november/">Lanyrd</a> | <a href="http://www.slideshare.net/asgrim1/low-latency-logging-brighton-php-18th-nov-2013">Slides</a></p>
 	</li>
 	<li>
 		<h3>Errors, Exceptions &amp; Logging (PHP Hampshire, 9th Oct '13)</h3>
 		<p>The talk is designed to give an entry-level introduction to how you should be handling errors, exceptions and how to effectively log in an application.</p>
-		<p><strong>Links:</strong> <a href="https://joind.in/9452">Joind.in</a> | <a href="http://www.youtube.com/watch?v=NnhkNhM3aDQ">YouTube</a> | <a href="http://www.slideshare.net/asgrim1/errors-exceptions-logging-php-hants-oct-13">Slides</a></p>
+		<p><strong>Links:</strong> <a href="https://joind.in/9452" class="joindin">Joind.in</a> | <a href="http://www.youtube.com/watch?v=NnhkNhM3aDQ">YouTube</a> | <a href="http://www.slideshare.net/asgrim1/errors-exceptions-logging-php-hants-oct-13">Slides</a></p>
 	</li>
 	<li>
 		<h3>Errors, Exceptions &amp; Logging (PHPNW13 Uncon, 5th Oct '13)</h3>
 		<p>A brief introduction to how to handle errors, exceptions and some effective ways to log them.</p>
-		<p><strong>Links:</strong> <a href="https://joind.in/9470">Joind.in</a> | <a href="http://www.slideshare.net/asgrim1/errors-exceptions-logging-phpnw13-uncon">Slides</a></p>
+		<p><strong>Links:</strong> <a href="https://joind.in/9470" class="joindin">Joind.in</a> | <a href="http://www.slideshare.net/asgrim1/errors-exceptions-logging-phpnw13-uncon">Slides</a></p>
 	</li>
 	<li>
 		<h3>Composer (PHP Hampshire, 11th Sep '13)</h3>
 		<p>A very quick introduction to the basics of Composer, what problems it solves, what it does, including a live demo.</p>
-		<p><strong>Links:</strong> <a href="https://joind.in/9341">Joind.in</a> | <a href="http://www.youtube.com/watch?v=nnDUSkvdvWg">YouTube</a> | <a href="http://www.slideshare.net/asgrim1/composer-tutorial-php-hants-sept-13-26138484">Slides</a></p>
+		<p><strong>Links:</strong> <a href="https://joind.in/9341" class="joindin">Joind.in</a> | <a href="http://www.youtube.com/watch?v=nnDUSkvdvWg">YouTube</a> | <a href="http://www.slideshare.net/asgrim1/composer-tutorial-php-hants-sept-13-26138484">Slides</a></p>
 	</li>
 </ul>
 
