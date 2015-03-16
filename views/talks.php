e0997abdc9a8501ac5341ac3bc878f5643d70431
James Titcumb <james@asgrim.com>
2015-03-16 17:09:33 +0000

    Update talks.php

--- views/talks.php
+++ views/talks.php
@@ -9,7 +9,21 @@ require_once('header.php');
 <p>Upcoming talks:</p>
 
 <ul>
-	<li>Nothing upcoming for now!</li>
+	<li>
+		<h3>Adding 1.21 Gigawatts to Applications with RabbitMQ (PHP Tour Luxembourg, 12th-13th May '15)</h3>
+		<p>As your application grows, you soon realise you need to break up your application into smaller chunks that talk to each other. You could just use web services to interact, or you could take a more robust approach and use the message broker RabbitMQ. In this talk, we will take a look at the techniques you can use to vastly enhance inter-application communication, learn about the core concepts of RabbitMQ, cover how you can scale different parts of your application separately, and modernise your development using a message-oriented architecture.</p>
+		<p><strong>Links:</strong> <a href="http://www.afup.org/pages/phptourluxembourg2015/">PHP Tour Luxembourg</a></p>
+	</li>
+	<li>
+		<h3>Adding 1.21 Gigawatts to Applications with RabbitMQ (Dutch PHP Conference, 25th-27th Jun '15)</h3>
+		<p>As your application grows, you soon realise you need to break up your application into smaller chunks that talk to each other. You could just use web services to interact, or you could take a more robust approach and use the message broker RabbitMQ. In this talk, we will take a look at the techniques you can use to vastly enhance inter-application communication, learn about the core concepts of RabbitMQ, cover how you can scale different parts of your application separately, and modernise your development using a message-oriented architecture.</p>
+		<p><strong>Links:</strong> <a href="http://www.phpconference.nl/schedule#conference-day-2/adding-121-gigawatts-applications-rabbitmq">Dutch PHP Conference</a></p>
+	</li>
+	<li>
+		<h3>Dip Your Toes in the Sea of Security (Dutch PHP Conference, 25th-27th Jun '15)</h3>
+		<p>Security is an enormous topic, and it’s really, really complicated. If you’re not careful, you’ll find yourself vulnerable to any number of attacks which you definitely don’t want to be on the receiving end of. This talk will give you just a taster of the vast array of things there is to know about security in modern web applications, such as writing secure PHP web applications and securing a Linux server. Whether you are writing anything beyond a basic brochure website, or even developing a complicated business web application, this talk will give you insights to some of the things you need to be aware of.</p>
+		<p><strong>Links:</strong> <a href="http://www.phpconference.nl/schedule#conference-day-1/dip-your-toes-sea-security">Dutch PHP Conference</a></p>
+	</li>
 </ul>
 
 <p>This is a list of talks I've given:</p>
