208b55cf0e1e07c1f00e85842834e5eeb97a9970
James Titcumb <james@asgrim.com>
2015-03-17 08:39:22 +0000

    Update talks.php

--- views/talks.php
+++ views/talks.php
@@ -12,7 +12,7 @@ require_once('header.php');
 	<li>
 		<h3>Adding 1.21 Gigawatts to Applications with RabbitMQ (PHP Tour Luxembourg, 12th-13th May '15)</h3>
 		<p>As your application grows, you soon realise you need to break up your application into smaller chunks that talk to each other. You could just use web services to interact, or you could take a more robust approach and use the message broker RabbitMQ. In this talk, we will take a look at the techniques you can use to vastly enhance inter-application communication, learn about the core concepts of RabbitMQ, cover how you can scale different parts of your application separately, and modernise your development using a message-oriented architecture.</p>
-		<p><strong>Links:</strong> <a href="http://www.afup.org/pages/phptourluxembourg2015/">PHP Tour Luxembourg</a></p>
+		<p><strong>Links:</strong> <a href="http://www.afup.org/pages/phptourluxembourg2015/sessions.php#1351">PHP Tour Luxembourg</a></p>
 	</li>
 	<li>
 		<h3>Adding 1.21 Gigawatts to Applications with RabbitMQ (Dutch PHP Conference, 25th-27th Jun '15)</h3>
