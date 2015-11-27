b77f155afa0c9b61e308c32c38459390640e6c35
James Titcumb <james@asgrim.com>
2015-11-27 13:39:21 +0000

    Display current search query in search box

--- src/Asgrim/Action/SearchAction.php
+++ src/Asgrim/Action/SearchAction.php
@@ -2,12 +2,14 @@
 
 namespace Asgrim\Action;
 
+use Elasticsearch\Common\Exceptions\TransportException;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Expressive\Template\TemplateRendererInterface as TemplateRenderer;
 use Asgrim\Service\PostService;
 use Asgrim\Service\SearchWrapper;
+use Zend\View\Model\ViewModel;
 
 class SearchAction
 {
@@ -49,17 +51,27 @@ class SearchAction
         $queryParams = $request->getQueryParams();
 
         if (!isset($queryParams['q']) || '' === trim($queryParams['q'])) {
-            return new HtmlResponse($this->template->render('app::search-error'));
+            return new HtmlResponse($this->template->render('app::search/no-query'));
         }
 
-        $rawResults = $this->searchWrapper->search($queryParams['q']);
+        try {
+            $rawResults = $this->searchWrapper->search($queryParams['q']);
+        } catch (TransportException $transportException) {
+            return new HtmlResponse($this->template->render('app::search/unavailable'));
+        }
 
         $posts = [];
         foreach ($rawResults as $rawResult) {
             $posts[] = $this->postService->fetchPostBySlug($rawResult['slug']);
         }
 
-        return new HtmlResponse($this->template->render('app::search-results', [
+        $layoutModel = new ViewModel([
+            'searchQuery' => $queryParams['q'],
+        ]);
+        $layoutModel->setTemplate('layout/default');
+
+        return new HtmlResponse($this->template->render('app::search/results', [
+            'layout' => $layoutModel,
             'query' => $queryParams['q'],
             'results' => $posts,
         ]));
