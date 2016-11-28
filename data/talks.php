08e48bc0a7f8b014a7b1b641a14814f4866cb28d
James Titcumb <james@asgrim.com>
2016-11-28 10:50:24 +0000

    Fix dates of PHPem unconf talks

--- data/talks.php
+++ data/talks.php
@@ -27,7 +27,7 @@ return [
     [
         'name' => 'Interpret this... (Part 1)',
         'type' => 'lightning',
-        'date' => new DateTime('2016-11-28'),
+        'date' => new DateTime('2016-11-26'),
         'event' => 'PHPem Unconference 2016',
         'abstract' => 'First half of a two-part talk. I\'ll show how the Lexer, Parser and AST work in PHP, introducing a little bit of compiler theory that helps explain.',
         'links' => [
@@ -37,7 +37,7 @@ return [
     [
         'name' => '...now write an interpreter (Part 2)',
         'type' => 'lightning',
-        'date' => new DateTime('2016-11-28'),
+        'date' => new DateTime('2016-11-26'),
         'event' => 'PHPem Unconference 2016',
         'abstract' => 'Second half of my two part series on interpreters. In this, I demonstrate how easy it is to write a very basic maths sum interpreter, including a live demo of adding a new language feature!',
         'links' => [
