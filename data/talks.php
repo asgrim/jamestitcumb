44ac904b3b287e711f645df16d39fb001edb7d7c
James Titcumb <james@asgrim.com>
2017-02-07 05:09:44 -0500

    Add PHP Serbia

--- data/talks.php
+++ data/talks.php
@@ -37,6 +37,16 @@ return [
     [
         'name' => 'Kicking off with Zend Expressive and Doctrine ORM',
         'type' => 'talk',
+        'date' => new DateTime('2017-05-27'),
+        'event' => 'PHP Srbija 2017',
+        'abstract' => $expressiveAbstract,
+        'links' => [
+            'PHP Srbija 2017' => ['url' => 'http://www.conf2017.phpsrbija.rs/schedule#kicking-off-with-zend-expressive-and-doctrine-orm'],
+        ],
+    ],
+    [
+        'name' => 'Kicking off with Zend Expressive and Doctrine ORM',
+        'type' => 'talk',
         'date' => new DateTime('2017-02-17'),
         'event' => 'PHP UK Conference 2017',
         'abstract' => $expressiveAbstract,
